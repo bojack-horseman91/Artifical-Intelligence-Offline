a1587f554e58c7cd67b83f70cc2217129fbbc69f
Sakib91 <birdhunterx91@gmail.com>
2023-01-22 12:27:26 +0600

    final

--- Offline 2/main.py
+++ Offline 2/main.py
@@ -5,23 +5,50 @@ from MainFiles.MinConflict import *
 from MainFiles.TimeTabling import *
 from ConstructiveHeuristics.ByLargestNumberOfConflicts import *
 from ConstructiveHeuristics.DeSaturation import *
-from ConstructiveHeuristics.ByLargestWeigthedDegree import *
+from ConstructiveHeuristics.ByLargestDegree import *
+from ConstructiveHeuristics.ByRandom import *
 
 
-FILE_NAME="car-f-92"
+problems=["car-f-92","car-s-91","yor-f-83","tre-s-92","kuf-s-93"]
+solvers=[timetable_by_largest_degree,timetableByDSaturAlgorithm,timetable_by_random_ordering,timetableByLargestStudentsWithConflict]
+class Solver:
+    def __init__(self,fileName) -> None:
+        self.FILE_NAME=fileName
+    def writeToReport(self,fileName,output):
+        with open(f"{fileName}.sol",'a') as writer:
+            for t in output:
+                writer.write(f"{t} ")
+            writer.write('\n')
+    def solve(self,file_name):
+        i=0
+        for solver in solvers:
+            i+=1
+            _courses=read_courses(file_name)
+            _students=read_students(file_name,_courses)
+            conflict_matrix=create_conflict_matrix(_courses,_students)
+            create_overlapping_courses(_courses,conflict_matrix)
+            # total_time_slots=do_timetabling(_courses)
+            
+             
+            if i==4:
+                total_time_slots=solver(_courses,_students)
+            else:
+                total_time_slots=solver(_courses)
+            print(total_time_slots)
+            before=calculate_average_penalty(_students)
+            print(before)
+            MinPenaltyReduction(_courses,_students,True)
+            after1=calculate_average_penalty(_students)
+            print(after1)
+            MinPenaltyReduction(_courses,_students,False)
+            after2=calculate_average_penalty(_students)
+            print(after2)
+            self.writeToReport(file_name,[before,after1,after2])
 
-def solve(file_name):
-    _courses=read_courses(file_name)
-    _students=read_students(file_name,_courses)
-    conflict_matrix=create_conflict_matrix(_courses,_students)
-    create_overlapping_courses(_courses,conflict_matrix)
-    # total_time_slots=do_timetabling(_courses)
-    # total_time_slots=timetableByDSaturAlgorithm(_courses)
-    # total_time_slots=timetableByLargestStudentsWithConflict(_courses,_students)
-    total_time_slots=timetable_by_largest_weighted_degree(_courses,_students)
-    print(total_time_slots)
+def main():
+    for problem in problems:
+        Solver(problem).solve(problem)
 
-    print(calculate_average_penalty(_students))
-    MinPenaltyReduction(_courses,_students,True)
-    print(calculate_average_penalty(_students))
-solve(FILE_NAME)
+
+if __name__ == '__main__':
+    main()
